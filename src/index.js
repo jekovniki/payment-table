811258542a2639002547d4773c562cd211ec2349
jekovniki <jekovniki95@gmail.com>
2023-01-02 15:46:45 +0200

    Setup-redux [+] react redux [+] tableOrder reducer; [+] orderTableAscending [=] refactor Table component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import allReducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
 );
 
